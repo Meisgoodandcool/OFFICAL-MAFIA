5e0bbe5529e2dfad5f88ce693303e5851805abff
Meisgoodandcool <137659904+Meisgoodandcool@users.noreply.github.com>
2026-03-25 20:06:01 -0400

    Update auth.js

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,10 +1,10 @@
 export default function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end()
   const { password } = req.body
-  if (password === process.env.2363564) {
+  if (password === process.env.HICOM_PASSWORD) {
     return res.json({ role: 'hicom' })
   }
-  if (password === process.env.52345673) {
+  if (password === process.env.STAFF_PASSWORD) {
     return res.json({ role: 'staff' })
   }
   return res.json({ role: null })
